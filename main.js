a04e1dba4278d0f2814b8be9befb117a0762efa5
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-20 13:33:57 +0530

    Done changes

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Video Modal Logic (supports multiple play buttons using data-video)
     // const videoModal = document.getElementById('videoModal');
-    const playBtns = document.querySelectorAll('.play-btn');
+    // const playBtns = document.querySelectorAll('.play-btn');
     const closeModal = document.querySelector('.close-modal');
     const yogaVideo = document.getElementById('yogaVideo');
 
