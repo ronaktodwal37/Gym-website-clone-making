a04a1e906709ae6135dc0b26545fcdb92f42ce64
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-08 23:57:22 +0530

    change

--- main.js
+++ main.js
@@ -54,10 +54,10 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Video Modal Logic (supports multiple play buttons using data-video)
-    // const videoModal = document.getElementById('videoModal');
-    // const playBtns = document.querySelectorAll('.play-btn');
-    // const closeModal = document.querySelector('.close-modal');
-    // const yogaVideo = document.getElementById('yogaVideo');
+    const videoModal = document.getElementById('videoModal');
+    const playBtns = document.querySelectorAll('.play-btn');
+    const closeModal = document.querySelector('.close-modal');
+    const yogaVideo = document.getElementById('yogaVideo');
 
     const hideModal = () => {
         if (!videoModal) return;
